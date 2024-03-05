9fab244574dc0ebd3b34cbb355f9a4b4d418ca5c
fabiocotta@fabiocotta.com.br <fabiocotta>
2024-03-05 13:58:52 -0300

    primeiro commit
    criação projeto

---
+++ lojakhipo.dpr
@@ -0,0 +1,14 @@
+program lojakhipo;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  UntPrincipal in 'UntPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
