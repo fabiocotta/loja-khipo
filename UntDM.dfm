a73372925e2b457d06246380d7a8689415fec252
fabiocotta@fabiocotta.com.br <fabiocotta>
2024-03-05 14:25:26 -0300

    segundo commit
    criação da unit dm

---
+++ UntDM.dfm
@@ -0,0 +1,8 @@
+object DataModule1: TDataModule1
+  Height = 312
+  Width = 604
+  object Conn: TFDConnection
+    Left = 64
+    Top = 72
+  end
+end
--- lojakhipo.dpr
+++ lojakhipo.dpr
@@ -3,12 +3,14 @@ program lojakhipo;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  UntPrincipal in 'UntPrincipal.pas' {frmPrincipal};
+  UntPrincipal in 'UntPrincipal.pas' {frmPrincipal},
+  UntDM in 'UntDM.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
